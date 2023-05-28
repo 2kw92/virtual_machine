c9ff20ef7fe70237c756f3e6779b099847d0e94e
Konstantin Kurochkin <2kw92@mail.ru>
2023-05-28 23:27:19 +0300

    add new machine

---
+++ terraform/postgres/postgres_patroni/variables.tf
@@ -0,0 +1,8 @@
+# variable.tf
+
+variable "ssh_key_private" {
+   default = "C:\\Users\\1\\.ssh\\mykeys\\yo"
+}
+variable "sql_dump" {
+   default = "C:\\Users\\1\\Documents\\dumps\\demo-medium-20170815.sql" 
+}
